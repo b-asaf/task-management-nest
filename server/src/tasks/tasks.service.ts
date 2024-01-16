57f60b8cf49eda3bf063d9d1135ff29a0207787d
asaf bar <assafbar81@gmail.com>
2024-01-16 08:35:38 +0200

    Define 1st endpoint - GetAllTasks

--- server/src/tasks/tasks.service.ts
+++ server/src/tasks/tasks.service.ts
@@ -1,4 +1,12 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class TasksService {}
+export class TasksService {
+  // temp solution till DB will be added
+  private tasks = [];
+
+  // by default methods are 'public'
+  getAllTasks() {
+    return this.tasks;
+  }
+}
