12824fabbd071801759c983649709cb4e62b72a5
asaf bar <assafbar81@gmail.com>
2024-01-16 09:16:57 +0200

    Adding getTaskById endpoint - error handling will be added later

--- server/src/tasks/tasks.controller.ts
+++ server/src/tasks/tasks.controller.ts
@@ -1,14 +1,22 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
 import { Task } from './task.model';
 import { TasksService } from './tasks.service';
+import { CreateTaskDto } from './dto/create-task.dto';
 
 @Controller('tasks')
 export class TasksController {
   constructor(private tasksService: TasksService) {}
 
+  // http://localhost:3000/tasks
   @Get()
   getAllTasks(): Task[] {
     return this.tasksService.getAllTasks();
+  }
+
+  // http://localhost:3000/tasks/9340293840934
+  @Get('/:id')
+  getTaskById(@Param('id') id: string): Task {
+    return this.tasksService.getTaskById(id);
   }
 
   // // Option A
@@ -19,14 +27,21 @@ export class TasksController {
   // }
 
   // // Option B
-  // // [GOOD!!!] - NestJS will cherry pick the defined properties inside the body behind the scenes
+  // // [Better!!!] - NestJS will cherry pick the defined properties inside the body behind the scenes
+  // @Post()
+  // createTask(
+  //   @Body('title') title: string,
+  //   @Body('description') description: string,
+  // ): Task {
+  //   // console.log('title', title);
+  //   // console.log('description', description);
+  //   return this.tasksService.createTask(title, description);
+  // }
+
+  // // Option C
+  // // [Good!!!] - Use Dto class instead
   @Post()
-  createTask(
-    @Body('title') title: string,
-    @Body('description') description: string,
-  ): Task {
-    // console.log('title', title);
-    // console.log('description', description);
-    return this.tasksService.createTask(title, description);
+  createTask(@Body() createTaskDto: CreateTaskDto): Task {
+    return this.tasksService.createTask(createTaskDto);
   }
 }
