b200f384db847fb0c69b9964b8232473a4d9faf1
asaf bar <assafbar81@gmail.com>
2024-01-16 09:02:29 +0200

    Create Task: 1. add create task logic in tasks Service. 2. expose createTask endpoint in tasks Controller

--- server/src/tasks/tasks.controller.ts
+++ server/src/tasks/tasks.controller.ts
@@ -1,4 +1,5 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
+import { Task } from './task.model';
 import { TasksService } from './tasks.service';
 
 @Controller('tasks')
@@ -6,7 +7,26 @@ export class TasksController {
   constructor(private tasksService: TasksService) {}
 
   @Get()
-  getAllTasks() {
+  getAllTasks(): Task[] {
     return this.tasksService.getAllTasks();
+  }
+
+  // // Option A
+  // // [BAD!!!] - because the body can contain information that is not needed and validation and cherry pick the relevant information
+  // @Post()
+  // createTask(@Body() body) {
+  //   console.log('body', body);
+  // }
+
+  // // Option B
+  // // [GOOD!!!] - NestJS will cherry pick the defined properties inside the body behind the scenes
+  @Post()
+  createTask(
+    @Body('title') title: string,
+    @Body('description') description: string,
+  ): Task {
+    // console.log('title', title);
+    // console.log('description', description);
+    return this.tasksService.createTask(title, description);
   }
 }
--- server/src/tasks/tasks.service.ts
+++ server/src/tasks/tasks.service.ts
@@ -1,12 +1,27 @@
 import { Injectable } from '@nestjs/common';
+import { Task, TaskStatus } from './task.model';
+import { v4 as uuid } from 'uuid';
 
 @Injectable()
 export class TasksService {
   // temp solution till DB will be added
-  private tasks = [];
+  private tasks: Task[] = [];
 
   // by default methods are 'public'
-  getAllTasks() {
+  getAllTasks(): Task[] {
     return this.tasks;
+  }
+
+  createTask(title: string, description: string): Task {
+    const task: Task = {
+      id: uuid(),
+      title,
+      description,
+      status: TaskStatus.OPEN,
+    };
+
+    this.tasks.push(task);
+
+    return task;
   }
 }
