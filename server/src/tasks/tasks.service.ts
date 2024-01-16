63fc063fd1e2bd29cb773b43e7e1f0f6c6cb6442
asaf bar <assafbar81@gmail.com>
2024-01-16 11:20:29 +0200

    error handling - delete task by id

--- server/src/tasks/tasks.service.ts
+++ server/src/tasks/tasks.service.ts
@@ -66,6 +66,8 @@ export class TasksService {
   }
 
   deleteTask(id: string): void {
+    // just for testing purposes
+    const found = this.getTaskById(id);
     this.tasks = this.tasks.filter((task) => task.id !== id);
   }
 
