80bc3306af30c81ed90b59fd323253f9839c7d4a
asaf bar <assafbar81@gmail.com>
2024-01-17 11:26:34 +0200

    Adding validators to user entity - username & password

--- server/src/auth/dto/auth-credentials.ts
+++ server/src/auth/dto/auth-credentials.ts
@@ -1,9 +1,21 @@
-import { IsNotEmpty } from 'class-validator';
+import { IsString, Matches, MaxLength, MinLength } from 'class-validator';
 
+// Regular expression explanation:
+// - Passwords will contain at least 1 upper case letter
+// - Passwords will contain at least 1 lower case letter
+// - Passwords will contain at least 1 number or special character
+// - There is no length validation (min, max) in this regex! -> using different validator
 export class AuthCredentialsDto {
-  @IsNotEmpty()
+  @IsString()
+  @MinLength(4)
+  @MaxLength(32)
   username: string;
 
-  @IsNotEmpty()
+  @IsString()
+  @MinLength(8)
+  @MaxLength(32)
+  @Matches(/((?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$/, {
+    message: 'Password is to week',
+  })
   password: string;
 }
