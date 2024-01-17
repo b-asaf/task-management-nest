b1aaaadbd8a1c1f77405c12073508e77d5a5bba5
asaf bar <assafbar81@gmail.com>
2024-01-17 15:21:08 +0200

    JWT 1. add JWT service using passportJS 2. generate accessToken on SignIn 2.1 JwtPayload interface add to keep JwtPayload consistent in the application

--- server/src/auth/auth.module.ts
+++ server/src/auth/auth.module.ts
@@ -1,13 +1,25 @@
 import { Module } from '@nestjs/common';
+import { JwtModule } from '@nestjs/jwt';
+import { PassportModule } from '@nestjs/passport';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { UsersRepository } from './users.repository';
 import { User } from './user.entity';
+import { JWT_EXPIRATION_TIME_SPAN } from './constant';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([User])],
+  imports: [
+    PassportModule.register({ defaultStrategy: 'jwt' }),
+    JwtModule.register({
+      secret: 'topSecret42', // temp solution
+      signOptions: {
+        expiresIn: JWT_EXPIRATION_TIME_SPAN, // 3600seconds -> 1 hour
+      },
+    }),
+    TypeOrmModule.forFeature([User]),
+  ],
   controllers: [AuthController],
   providers: [AuthService, UsersRepository],
 })
--- server/src/auth/constant.ts
+++ server/src/auth/constant.ts
@@ -1,1 +1,4 @@
 export const DUPLICATE_USERNAME_CODE = '23505';
+
+// one hour - 3600 seconds
+export const JWT_EXPIRATION_TIME_SPAN = 3600;
